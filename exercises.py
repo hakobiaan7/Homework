f5e575a48a55b9dbea9cf35173393ff2f2ebb97d
hakobiaan7 <karlen.hakobyan.98@mail.ru>
2022-04-01 07:40:52 -0700

    HomeWork

---
+++ exercises.py
@@ -0,0 +1,65 @@
+'''Exercise 1'''
+
+# def calc(a, b, n):
+#     if n == '+':
+#         return a + b
+#     elif n == '-':
+#         return a - b
+#     elif n == '*':
+#         return a * b
+#     elif n == '/':
+#         return a / b
+#     elif n == '%':
+#         return a % b
+# num1 = int(input('Input num1 - '))
+# num2 = int(input('Input num2 - '))
+# act = input('Input action - ')
+# print('Your Output is - ', calc(num1, num2, act))
+
+'''Exercise 2'''
+
+# def max(a, b):
+#     if a == b:
+#         return 'Numbers Are Equal'
+#     elif a > b:
+#         return a, 'is max'
+#     elif a < b:
+#         return b, 'is max'
+# num1 = int(input('Input num1 - '))
+# num2 = int(input('Input num2 - '))
+# print(max(num1, num2))
+
+'''Exercise 3'''
+
+# def sum(a, b):
+#     return a + b
+# num1 = int(input('Input num1 - '))
+# num2 = int(input('Input num2 - '))
+# print(sum('Sum = ', num1, num2))        
+
+'''Exercise 4'''
+
+# def mult(a, b):
+#     return a * b
+# num1 = int(input('Input num1 - '))
+# num2 = int(input('Input num2 - '))
+# print('Multiple = ', mult(num1, num2))
+
+'''Exercise 5'''
+
+'''Sra pahanjy chem haskanum vonc karam tarin tiv gumarem?'''
+
+'''Exercise 6''' '''Ab jan verjum none a tpum chgitem inchi'''
+
+# def work(mydict):
+#     mylist1 = []
+#     for i in mydict.values():
+#         mylist1.append(i)
+#     for i in mylist1:
+#         if i < 16:
+#             print(i, 'Too young!')
+#         else:
+#             print(i, 'Get ready!')
+# dict1 = {'Arman' : 16, 'Arsen' : 15, 'Artur' : 21}
+# print(work(dict1))
+
